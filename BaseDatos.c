1f0eb8fa88817ab924c87af4e94a0dc549a24fbf
keux00 <49764449+keux00@users.noreply.github.com>
2020-06-11 23:24:39 +0200

    Versión 2
    FUNCIONAL:
    - Agregar Empleado
    - Borrar Empleado
    - Modificar Empleado
    - Listar empleados
    - Salir del programa
    
    NO FUNCIONAL:
    - Listar proyectos
    - Asignar proyecto
    - Dar de baja proyecto
    - Guardar fichero
    - Cargar desde fichero
    
    Comentario: Sigue habiendo código que está en desarrollo y de ahí que esté "oculto" con los /* y */ ya que no ejecuta bien con esa parte activa. Seguiré actualizando conforme tenga más novedades

--- BaseDatos.c
+++ BaseDatos.c
@@ -1,24 +1,26 @@
 /*------------------------------------------------------------- 
-Menú de gestión de empresas por Keux00
+MenÃº de gestiÃ³n de empresas por Keux00
 ---------------------------------------------------------------*/
 
-// Incluímos las librerias que consideremos necesarias
+// IncluÃ­mos las librerias que consideremos necesarias
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Definimos las variables
 
+int h;
 int i;
 int n=0;
 int opc;
 int pos;
-int h=0;
 int continuar='Y';
 int numident;
 int empleados_registrados=0;
+int proyectos_registrados=0;
 
 // Hacemos y preparamos las estructuras y sus datos correspondientes
 
@@ -26,6 +28,7 @@ struct proyecto
 {
 char nombreproyecto[60];
 int presupuestoproyecto;
+int eliminarp;
 };
 
 struct empleado
@@ -39,46 +42,19 @@ struct proyecto proyecto_empleado[10];
 int eliminar;
 }lista[500];
 
-// Función para revisar errores (nos servirá próximamente)
-
-int error(){
-	
-	int j;
-	char reg;
-	for (j=0; j<n; j++){
-		if(lista[j].eliminar == 1){
-			printf("\n Este empleado ya habia sid registrado, desea volver a registrarlo (Y/N): ");
-			reg=getch();
-			reg=toupper(reg);
-			if(reg=='Y'){
-				printf("\n Empleado registrado de nuevo!");
-				lista[j].eliminar=0;
-				empleados_registrados++;
-				j=n;
-			}
-		}
-		else{
-			printf("\n Empleado registrado");
-			j=n;
-		}
-	}
-	system("PAUSE");
-	menu();
-}
-
-// Función para recoger los datos del empleado
+// FunciÃ³n para recoger los datos del empleado
 
 int pedir_datos(){
 
 	int a;
 	do {
 
-		printf("\n INTRODUZCA LOS DATOS DEL CLIENTE \n");
-		
+		printf("\n Introduzca los datos del empleado");
+		printf("\n");
 		printf("\n Nombre: "); scanf("%s",&lista[i].nombre);
 		printf("\n Apellido: "); scanf("%s",&lista[i].apellido);
 		printf("\n Num. identif: "); scanf("%d",&lista[i].numidentif);
-		//---------------------------REVISAR QUE NO EXISTA--------------------------
+		//---------------------------Revisar que no exista--------------------------
 		for (a=0; a<n; a++){
 			if(lista[i].numidentif == lista[a].numidentif){
 				error();
@@ -88,8 +64,7 @@ int pedir_datos(){
 		}
 		//---------------------------------------------------------------------------
 		printf("\n Edad: "); scanf("%d",&lista[i].edad);
-		printf("\nA%co de nacimiento del empleado: "); scanf("%d",&lista[i].anyo)
-		printf("\nN%cmero de proyectos en los que trabaja: "); scanf("%d",&lista[i].proyectos)
+		printf("\n A%co de nacimiento del empleado: ",164); scanf("%d",&lista[i].anyo);
 		lista[i].eliminar=0;
 		n++;
 		i++;
@@ -99,36 +74,24 @@ int pedir_datos(){
 	}while(toupper(continuar)!='N');
 }
 
-// Función para mostrar los datos del empleado
+// FunciÃ³n para mostrar los datos del empleado
 
 void mostrar_datos(){
 	
-	while (j<DIM){
-		
-		if(lista[j].numidentif!=0){
-		
-		printf("\n");
-		printf("\nDatos del empleado: %i",j+1);
-		printf("\n-----------------------------------");
-		printf("\nN%cmero indentificativo: %i",163,lista[j].numidentif);
-		printf("\nNombre del empleado: %s",lista[j].nombre);
-		printf("\nApellido del empleado: %s",lista[j].apellido);
-		printf("\nEdad del empleado: %i",lista[j].edad);
-		printf("\nA%co de nacimiento del empleado: %i",164,lista[j].anyo);
-		printf("\nN%cmero de proyectos en los que trabaja: %i",163,lista[j].proyectos);
-		printf("\n");
-		}
-		
-		else
-		{
-			break;
+	printf("\n Listado de empleados");
+	printf("\n");
+	if(empleados_registrados==0) printf("\n No se ha registrado ningun empleado");
+	for (i=0; i<n;i++){
+		if (lista[i].eliminar==0 && n>0){
+			printf("\n\t__________________________________________________________________\n\n");
+			printf("\n\tNombre\t\tApellido\t\tNum. Identificador\t\tEdad\t\tA%co Nacimiento\n\n ",164);
+			printf("\n\t%s \t\t%s \t\t\t%d \t\t\t\t%d \t\t\t%d \n\n" , lista[i].nombre, lista[i].apellido, lista[i].numidentif, lista[i].edad, lista[i].anyo);
 		}
 	}
-	
-	printf("\n");
+	getch();
 }
 
-// Función para eliminar empleados
+// FunciÃ³n para eliminar empleados
 
 void eliminar_empleado(){
 	
@@ -149,7 +112,7 @@ void eliminar_empleado(){
 		printf("\n Apellido: %s", lista[pos].apellido);
 		printf("\n Num. identif: %d", lista[pos].numidentif);
 		printf("\n Edad: %d", lista[pos].edad);
-		printf("\nA%co de nacimiento del empleado: %i",164,lista[pos].anyo);
+		printf("\n A%co de nacimiento del empleado: %i",164,lista[pos].anyo);
 		printf("\n Desea Eliminar Registro (Y/N): ");
 		scanf("%s",&opc);
 		if (toupper(opc)=='Y'){
@@ -166,9 +129,9 @@ void eliminar_empleado(){
 	getch();
 }
 
+// FunciÃ³n para modificar datos de empleados
+
 int modificar_datos(){
-	
-	int modificar (){
 	
 	bool empleado_no_registrado=false;
 	int a;
@@ -181,14 +144,14 @@ int modificar_datos(){
 		printf ("\n Introduzca el num. identif del empleado a modificar: ");
 		scanf("%d",&numident);
 		for (i=0; i<n;i++){
-			if(numident == empleado[i].numidentif){
+			if(numident == lista[i].numidentif){
 
 				printf("\n Modificar los datos del empleado");
 				printf("\n Nombre: %s", lista[i].nombre);
 				printf("\n Apellido: %s", lista[i].apellido);
 				printf("\n Num. identif: %d", lista[i].numidentif);
 				printf("\n Edad: %d", lista[i].edad);
-				printf("\nA%co de nacimiento del empleado: %i",164,lista[i].anyo);
+				printf("\nA%co de nacimiento del empleado: %d",164,lista[i].anyo);
 				
 				printf("\n Datos a modificar:");
 				printf("\n Nombre: ");
@@ -196,7 +159,7 @@ int modificar_datos(){
 				printf("\n Apellido: ");
 				scanf("%s",&lista[400].apellido);
 				printf("\n Num. identif: "); scanf("%d",&lista[400].numidentif);
-				//---------------------------REVISAR QUE NO EXISTA--------------------------
+				//---------------------------Revisar que no exista--------------------------
 				for (a=0; a<n-1; a++){
 					if(lista[400].numidentif == lista[a].numidentif){
 						error();
@@ -227,59 +190,71 @@ int modificar_datos(){
 	if(empleado_no_registrado == true) printf("\n Empleado no registrado ");
 	getch();
 }
-}
+
+// Funcion para listar proyectos de un empleado
 
 void listar_proyectos(){
-	
-	for (l=0;l<lista[i].proyectos;l++)
- 		
-		 {
-		 	printf("\n");
- 			printf("\n Proyecto n%cmero: %i",163,l+1);
- 			printf("\n");
- 			//printf("\n Nombre del proyecto: %s",lista[i].nombreproyecto);
- 			//printf("\n Presupuesto del proyecto: %i",lista[i].presupuestoproyecto);
-		 }
-	
+/*
+printf("\n Listado de proyectos");
+	printf("\n");
+	if(proyectos_registrados==0) printf("\n No se ha registrado ningun proyecto");
+	for (i=0; i<n;i++){
+		if (proyecto[i].eliminarp==0 && n>0){
+			printf("\n\t__________________________________________________________________\n\n");
+			printf("\n Proyecto numero %d: ",i+1);
+			printf("%s" ,proyecto[i].nombreproyecto);
+			printf("\n Presupuesto del proyecto: %d",proyecto[i].presupuestoproyecto);
+		}
+	}
+	getch();
+	*/
 }
+
+// FunciÃ³n para asignar proyectos a un empleado
 
 void asignar_proyecto(){
 	
-/*	for(i=0;i<DIM;i++){
+	/*
+		bool proyecto_no_registrado=false;
 	
-	printf("Introduce el n%cmero identificativo del empleado: ",163);
-	scanf("%u",&buscarnumident);
-	
-	if (lista[i].numidentif==buscarnumident){
+		printf ("\n Introduzca el num. identif del empleado: ");
+		scanf("%d",&numident);
 		
-	fflush(stdin);
- 		printf("\nIntroduce el n%cmero de proyectos en los que trabaja: ",163);
- 		scanf("%i",&lista[i].proyectos);
- 		
-	if (lista[i].proyectos>10)
-		{
- 		printf("\nError, introduce m%cximo 10 proyectos",160);
- 		printf("\nIntroduce el n%cmero de proyectos en los que trabaja: ",163);
- 		scanf("%i",&lista[i].proyectos);
- 		}
+		for (i=0; i<n;i++){
+			if(numident == lista[i].numidentif){
+				
+				for (h=0;h<lista[i].proyecto_empleado;h++)
+				{
+				printf("\n Proyectos actuales: ");
+				printf("\n Proyecto numero %d: ",h+1);
+				printf("%s", proyecto[h].nombreproyecto);
+				printf("\n Presupuesto del proyecto: %d",proyecto[h].presupuestoproyecto);
+				}
+				
+				printf("\n Nuevo proyecto: ");
+				printf("\n Nombre: ");
+				scanf("%s",&proyecto[400].nombreproyecto);
+				printf("\n Presupuesto: ");
+				scanf("%d",&proyecto[400].presupuestoproyecto);
+			
+				strcpy(proyecto[i].nombreproyecto, proyecto[400].nombreproyecto);
+				proyecto[i].presupuestoproyecto = proyecto[400].presupuestoproyecto;
+				
+				printf("\n Datos modificados exitosamente!");
+				proyecto[i].eliminarp=0;
+				proyecto_no_registrado=false;
+				i=n;
+			}
+			else proyecto_no_registrado=true;
+		}
 	
-	for (l=0;l<lista[i].proyectos;l++)
- 		{
- 			printf("\n Proyecto n%cmero: %i",163,l+1);
- 			printf("\n");
- 			printf("\n Introduce el nombre del proyecto: ");
- 			//scanf("%s",&lista[i].nombreproyecto);
- 			printf("\n Introduce el presupuesto del proyecto: ");
- 			//scanf("%i",&lista[i].presupuestoproyecto);
-		 }
-	}
-	
-	break;
-}
-*/
+	if(proyecto_no_registrado == true) printf("\n Proyecto no registrado ");
+	getch();
+	*/
 }
 
 void darbaja_proyecto(){
+	
 }
 
 void guardar_fichero(){
@@ -287,8 +262,8 @@ void guardar_fichero(){
 
 void cargar_fichero(){
 }
- 
-// Función menú
+
+// FunciÃ³n menÃº
 
 void menu(){
 	
@@ -329,7 +304,34 @@ int opcion;
 	}
 }
 
-// Función principal que contiene el menú y los submenús
+// FunciÃ³n para revisar errores (nos servirÃ¡ prÃ³ximamente)
+
+int error(){
+	
+	int j;
+	char reg;
+	for (j=0; j<n; j++){
+		if(lista[j].eliminar == 1){
+			printf("\n Este empleado ya habia sido registrado, desea volver a registrarlo (Y/N): ");
+			reg=getch();
+			reg=toupper(reg);
+			if(reg=='Y'){
+				printf("\n Empleado registrado de nuevo!");
+				lista[j].eliminar=0;
+				empleados_registrados++;
+				j=n;
+			}
+		}
+		else{
+			printf("\n Empleado registrado");
+			j=n;
+		}
+	}
+	system("PAUSE");
+	menu();
+}
+
+// FunciÃ³n principal que contiene el menÃº y los submenÃºs
 
 int main()
 {
